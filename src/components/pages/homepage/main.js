e4e0faa62b4584fd591ce9d989115089e00f96be
Unknown <akiyamka@gmail.com>
2017-05-06 17:00:54 +0300

    starting animation

--- src/components/pages/homepage/main.js
+++ src/components/pages/homepage/main.js
@@ -10,27 +10,32 @@ export default {
       skills: [
         { 
           header: 'Нестандартные задачи',
-          content: 'Разработка дополнений для WordPress, сложные системы расчета стоимости заказа, прокладка маршрута и прочие непростые задачи - <span> это как раз то что мы любим </span>'
+          content: 'Разработка дополнений для WordPress, сложные системы расчета стоимости заказа, прокладка маршрута и прочие непростые задачи - <span> это как раз то что мы любим </span>',
+          animation: this.animRing
         },
         {
           header: 'Продвинутый Front-end',
           content: 'Полноценные <span>SPA</span> и <span>PWA</span> под Ваши задачи на базе vue.js и сопуствущего стека технологий',
+          animation: this.animFront
         },
         {
           header: 'Чатботы',
           content: 'Системы оповещения, оформления заказа, отлеживания статуса, опросов и прочего на базе чат-платформы <span>Telegram</span>',
+          animation: this.animBot
         },
-      ]
+      ],
     }
   },
   methods: {
     runAnim (key) {
-      var resolver = [ this.animRing, this.animFront, this.animBot ];
-      resolver[key]()
+      this.skills[key].animation(true)
     },
-    animBot () {
-      var frontIcon = document.getElementById('bot-icon')
+    stopAnim (key) {
+      this.skills[key].animation(false)
+    },
+    animBot (swither) {
 
+      var frontIcon = document.getElementById('bot-icon')
       var lines = frontIcon.querySelectorAll('.cls-line')
       var lines2 = frontIcon.querySelectorAll('.cls-line-secondary')
       var rects = frontIcon.querySelectorAll('.cls-rect')
@@ -41,11 +46,14 @@ export default {
       var drawRect = KUTE.allFromTo(rects, {opacity: 0}, {opacity: 0.2}, {duration: 2000, easing: 'easingQuarticInOut', offset: 50})
       var drawFilled = KUTE.allFromTo(filled, {opacity: 0}, {opacity: 1}, {duration: 1000, easing: 'easingQuarticInOut', offset: 50})
 
-      drawRect.chain(drawFilled);
-      
-      drawLine.start();
-      drawLine2.start();
-      drawRect.start();
+
+      if (swither) {
+        drawRect.chain(drawFilled);
+        drawLine.start();
+        drawLine2.start();
+        drawRect.start();
+      }
+
     },
     animFront () {
       var frontIcon = document.getElementById('front-icon')
@@ -58,7 +66,7 @@ export default {
       drawLine.start();
       drawRect.start();
     },
-    animRing () {
+    animRing (swither) {
       var ringIcon = document.getElementById('ring-icon')
       var lines = ringIcon.querySelectorAll('.cls-line')
       var longDash = ringIcon.querySelectorAll('.cls-longDash')
@@ -66,45 +74,168 @@ export default {
       var wide = ringIcon.querySelectorAll('.cls-wide')
       var lines2 = ringIcon.querySelectorAll('.cls-line-secondary')
       var lines3 = ringIcon.querySelectorAll('.cls-line3')
+      var glow = ringIcon.querySelectorAll('.cls-glow')
+      var glowImg = ringIcon.querySelectorAll('.cls-glow-img')
+
+      // Draw animation
 
       var drawLine = KUTE.allFromTo(lines, {
-        draw: '0 0',
-        svgTransform: { rotate: 0 }
-      },
-      {
-        draw: '0% 100%',
-        svgTransform: { rotate: -90}
-      },
-      { duration: 3000,
-        easing: 'easingQuarticInOut',
-        offset: 100 
-      })
-      // var rotateLine = KUTE.allTo(lines, {rotate: 120}, {offset: 20, duration: 500} )
+          draw: '0 0',
+          svgTransform: { rotate: 0 }
+        },
+        {
+          draw: '0% 100%',
+          svgTransform: { rotate: -90}
+        },
+        {
+          duration: 3000,
+          easing: 'easingQuarticInOut',
+          offset: 100 
+        }
+      )
+
       var drawLine3 = KUTE.allFromTo(lines3, {
-        draw: '-150% -150%',
-        svgTransform: { rotate: 0 }
-      },
-      {
-        draw: '0 100%',
-        svgTransform: { rotate: 90 }
-      },
-      { duration: 3500,
-        easing: 'easingQuarticInOut',
-        offset: 100
-      })
+          draw: '-150% -150%',
+          svgTransform: { rotate: 0 }
+        },
+        {
+          draw: '0 100%',
+          svgTransform: { rotate: 90 }
+        },
+        {
+          duration: 3500,
+          easing: 'easingQuarticInOut',
+          offset: 100
+        }
+      )
 
-      var rotateLongDash = KUTE.allTo(longDash,{
-        svgTransform: { rotate: 120 }
-      },
-      { duration: 3000,
-        easing: 'easingQuarticInOut',
-        offset: 100
-      })
+      var drawLongDash = KUTE.allFromTo(longDash, {
+          opacity: 0
+        },
+        {
+          opacity: 1
+        },
+        {
+          duration: 3000,
+          easing: 'linear',
+        }
+      )
 
-      drawLine.start();
-      drawLine3.start();
-      rotateLongDash.start();
-      // rotateLine.start();
+      var drawShortDash = KUTE.allFromTo(shortDash, {
+          opacity: 0
+        },
+        {
+          opacity: 1
+        },
+        {
+          duration: 1500,
+          easing: 'linear',
+        }
+      )
+      
+      var drawWide = KUTE.allFromTo(wide, {
+          opacity: 0
+        },
+        {
+          opacity: 0.3
+        },
+        {
+          duration: 1500,
+          easing: 'linear',
+        }
+      )
+
+      var drawGlow = KUTE.allFromTo(glow, {
+          opacity: 0
+        },
+        {
+          opacity: 1
+        },
+        {
+          duration: 500,
+          easing: 'easingQuarticOut',
+        }
+      )
+
+      // Cycled animation
+      
+      var loopLongDash = () => setTimeout( () => rotateLongDash.start(), 50 );
+
+      var rotateLongDash = KUTE.allTo(longDash, {
+          svgTransform: { rotate: 360 }
+        },
+        {
+          complete: loopLongDash,
+          duration: 6000,
+          easing: 'easingCubicInOut',
+          yoyo: true,
+          repeat: 1
+        }
+      )
+
+      var loopShortDash = () => setTimeout( () => rotateShortDash.start(), 50 );
+
+      var rotateShortDash = KUTE.allFromTo(shortDash, {
+          svgTransform: { rotate: 0 }
+        },
+        {
+          svgTransform: { rotate: 360 }
+        },
+        {
+          complete: loopShortDash,
+          duration: 1000,
+          easing: 'linear ',
+          yoyo: false,
+          repeat: 1
+        }
+      )
+
+      var loopWide = () => setTimeout( () => pulseWide.start(), 50 );
+
+      var pulseWide =  KUTE.allFromTo(wide, {
+          opacity: 0.3
+        },
+        {
+          opacity: 0
+        },
+        {
+          complete: loopWide,
+          duration: 3000,
+          easing: 'easingCubicInOut',
+          yoyo: true,
+          repeat: 1
+        }
+      )
+
+      var loopGlow = () => setTimeout( () => pulseGlow.start(), 50 );
+
+      var pulseGlow =  KUTE.allFromTo(glowImg, {
+          opacity: 0
+        },
+        {
+          opacity: 1
+        },
+        {
+          complete: loopGlow,
+          duration: 3000,
+          easing: 'easingCubicInOut',
+          yoyo: true,
+          repeat: 1
+        }
+      )
+      // Start animation
+
+      if (swither) {
+        drawLine.start();
+        drawLine3.start();
+        drawLongDash.start().chain(rotateLongDash);
+        drawShortDash.start()
+        rotateShortDash.start()
+        drawWide.start().chain(pulseWide);
+        drawGlow.start().chain(pulseGlow);
+        
+      }
+
     }
 
   },
@@ -112,5 +243,3 @@ export default {
 
   }
 }
-
-
